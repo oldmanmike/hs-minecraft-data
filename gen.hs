efe5fbcd322ecc178a808a323047a6bbea616a20
oldmanmike <oldmanmike.dev@gmail.com>
2016-03-29 19:56:38 -0400

    Version extraction

--- gen.hs
+++ gen.hs
@@ -1,2 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+
+import            Data.Aeson
+import qualified  Data.ByteString as B
+import            Data.Data
+import            GHC.Generics
+
+type ExtractedVersionList = [String]
+
+data ExtractedVersion = ExtractedVersion
+  { version :: Int
+  , minecraftVersion :: String
+  , majorVersion :: String
+  } deriving (Show,Eq,Generic,Data,Typeable)
+
+instance FromJSON ExtractedVersion
+instance ToJSON ExtractedVersion
+
 main :: IO ()
-main = putStrLn "Let's generate some data!"
+main = do
+  rawVersionList <- B.readFile "minecraft-data/data/common/versions.json"
+  let versionList = eitherDecodeStrict' rawVersionList :: Either String [String] 
+  case versionList of
+    Right list -> do
+      let versionPaths =
+            map (\s -> "minecraft-data/data/"++s++"/version.json") list
+      rawList <- mapM B.readFile versionPaths
+      let x = (mapM eitherDecodeStrict' rawList) :: Either String [ExtractedVersion]
+      case x of
+        Right lst -> print lst
+        Left err -> print err
+    Left err -> print err
