59bdf1072ca672ebb17eb06a4d6d614f2ff21c34
oldmanmike <oldmanmike.dev@gmail.com>
2016-04-02 21:47:00 -0400

    Remove Common

--- src/Data/Minecraft/Common.hs
+++
@@ -1,7 +0,0 @@
-module Data.Minecraft.Common
-  ( module Data.Minecraft.Common.ProtocolVersions
-  , module Data.Minecraft.Common.Versions
-  ) where
-
-import Data.Minecraft.Common.ProtocolVersions
-import Data.Minecraft.Common.Versions
